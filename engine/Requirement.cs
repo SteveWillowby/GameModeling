0887b7981cfbf39daee850d1bfe29e2e9660b7ce
SteveWillowby <jhibshma@andrew.cmu.edu>
2016-10-29 19:54:45 -0400

    Engine: Preliminary work on Requirement parsing.

--- engine/Requirement.cs
+++ engine/Requirement.cs
@@ -2,28 +2,77 @@ using System;
 
 public class Requirement
 {
-    protected int numObjects;
+    protected int[] indices;
     protected Func<Object[], bool> evaluator;
 
     public bool Met(Object[] o)
     {
-        return o.Length == numObjects && evaluator(o);
+        return evaluator(o);
     }
 
-    public int GetNumObjects()
+    public Requirement(string command, string[] parameters, string[] types)
     {
-        return numObjects;
+        switch(command)
+        {
+            case "in":
+                In(parameters);
+                break;
+            case "contains":
+                Contains(parameters);
+                break;
+            default:
+                throw new Exception(command + " is not a valid requirement");
+                break;
+        }
     }
 
-    public Requirement()
+    protected void SetIndices(int[] i, int size)
     {
-        numObjects = 0;
-        evaluator = o => true;
+        if(i.Length != size)
+        {
+            throw new Exception("Bad requirement initialization list");
+        }
+        indices = new int[size];
+        for(int j = 0; j < size; j++)
+        {
+            indices[j] = i[j];
+        }
     }
 
-    public Requirement(Func<Object[], bool> e, int n)
+    protected void In(string[] parameters, string[] types)
     {
-        numObjects = n;
-        evaluator = e;
+        int len = parameters.Length;
+        if(len == 2) //a in b
+        {
+            
+        }
+        else if(len == 3)
+        {
+            
+        }
+        else if(len == 4)
+        {
+            if(parameters[2] == "<")
+            {
+                
+            }
+            else if(parameters[2] == ">")
+            {
+                
+            }
+            else
+            {
+                throw new Exception("Invalid third parameter for in");
+            }
+        }
+        else
+        {
+            throw new Exception("Invalid number of parameters");
+        }
+    }
+
+    protected void Contains(string[] parameters)
+    {
+        
     }
 }
